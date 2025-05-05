48ea6928f148401d8cbb83f3f576681bc7d1b387
DevMatheusMoura <matheussilvamouras@gmail.com>
2025-05-04 22:47:38 -0300

    inicia o projeto

---
+++ src/main/java/br/com/academy/wakanda/cadastro_saude/CadastroSaudeApplication.java
@@ -0,0 +1,13 @@
+package br.com.academy.wakanda.cadastro_saude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroSaudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroSaudeApplication.class, args);
+	}
+
+}
