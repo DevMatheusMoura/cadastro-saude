2edc6928763920230f68d8f4b569b899488ab2f5
DevMatheusMoura <matheussilvamouras@gmail.com>
2025-05-05 02:58:07 -0300

    implementa api de teste

--- src/main/java/br/com/academy/wakanda/cadastro_saude/CadastroSaudeApplication.java
+++ src/main/java/br/com/academy/wakanda/cadastro_saude/CadastroSaudeApplication.java
@@ -2,9 +2,19 @@ package br.com.academy.wakanda.cadastro_saude;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping
 @SpringBootApplication
 public class CadastroSaudeApplication {
+
+	@GetMapping
+	public String getHomTeste() {
+		return "Cadastro Saude - API Home";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CadastroSaudeApplication.class, args);
