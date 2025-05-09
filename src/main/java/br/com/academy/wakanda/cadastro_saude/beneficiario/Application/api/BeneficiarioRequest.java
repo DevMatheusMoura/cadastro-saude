20edfd1d088945dccbdbebd3bae126b3d2ebb572
DevMatheusMoura <matheussilvamouras@gmail.com>
2025-05-09 02:30:49 -0300

    Implementa request e response

--- src/main/java/br/com/academy/wakanda/cadastro_saude/beneficiario/Application/api/BeneficiarioRequest.java
+++ src/main/java/br/com/academy/wakanda/cadastro_saude/beneficiario/Application/api/BeneficiarioRequest.java
@@ -1,4 +1,17 @@
 package br.com.academy.wakanda.cadastro_saude.beneficiario.Application.api;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Value;
+import java.time.LocalDate;
+
+@Value
 public class BeneficiarioRequest {
+    @NotBlank(message = "Informe o seu nome completo")
+    private String nomeCompleto;
+    @NotBlank(message = "Informe um telefone válido com o DDD")
+    private String telefone;
+    @NotNull(message = "Informe a sua data de nascimento")
+    private LocalDate dataNascimento;
+
 }
--- src/main/java/br/com/academy/wakanda/cadastro_saude/beneficiario/Application/api/BeneficiarioResponse.java
+++ src/main/java/br/com/academy/wakanda/cadastro_saude/beneficiario/Application/api/BeneficiarioResponse.java
@@ -1,4 +1,8 @@
 package br.com.academy.wakanda.cadastro_saude.beneficiario.Application.api;
+import lombok.Value;
+import java.util.UUID;
 
+@Value
 public class BeneficiarioResponse {
+    private UUID idBeneficiario;
 }
