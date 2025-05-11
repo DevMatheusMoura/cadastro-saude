4bf660f70de91f973f76e3c2ed06043ba7c4dbf1
DevMatheusMoura <matheussilvamouras@gmail.com>
2025-05-11 11:06:39 -0300

    Implementa fluxo da api. Teste ok

--- src/main/java/br/com/academy/wakanda/cadastro_saude/beneficiario/Application/api/BeneficiarioAPI.java
+++ src/main/java/br/com/academy/wakanda/cadastro_saude/beneficiario/Application/api/BeneficiarioAPI.java
@@ -1,8 +1,11 @@
 package br.com.academy.wakanda.cadastro_saude.beneficiario.Application.api;
 
+import br.com.academy.wakanda.cadastro_saude.beneficiario.domain.Beneficiario;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/beneficiario")
@@ -10,4 +13,8 @@ public interface BeneficiarioAPI {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     BeneficiarioResponse postBeneficiario(@Valid @RequestBody BeneficiarioRequest BeneficiarioRequest);
+
+    @GetMapping
+    @ResponseStatus(code = HttpStatus.OK)
+    List<BeneficiarioListResponse> getTodosBeneficiarios();
 }
