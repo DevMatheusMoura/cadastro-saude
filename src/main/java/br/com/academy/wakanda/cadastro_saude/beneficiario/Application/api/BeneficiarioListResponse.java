1c83c9d4ca642b17ee8678c7e2622bec48ca9145
DevMatheusMoura <matheussilvamouras@gmail.com>
2025-05-12 02:22:21 -0300

    Implementa fluxo repository. Teste ok

--- src/main/java/br/com/academy/wakanda/cadastro_saude/beneficiario/Application/api/BeneficiarioListResponse.java
+++ src/main/java/br/com/academy/wakanda/cadastro_saude/beneficiario/Application/api/BeneficiarioListResponse.java
@@ -1,6 +1,9 @@
 package br.com.academy.wakanda.cadastro_saude.beneficiario.Application.api;
 
+import br.com.academy.wakanda.cadastro_saude.beneficiario.domain.Beneficiario;
+
 import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 public class BeneficiarioListResponse {
@@ -8,4 +11,8 @@ public class BeneficiarioListResponse {
     private String nomeCompleto;
     private String telefone;
     private LocalDate dataNascimento;
+
+    public static List<BeneficiarioListResponse> converte(List<Beneficiario> beneficiarios) {
+        return null;
+    }
 }
--- src/main/java/br/com/academy/wakanda/cadastro_saude/beneficiario/Application/repository/BeneficiarioRepository.java
+++ src/main/java/br/com/academy/wakanda/cadastro_saude/beneficiario/Application/repository/BeneficiarioRepository.java
@@ -1,7 +1,12 @@
 package br.com.academy.wakanda.cadastro_saude.beneficiario.Application.repository;
 
+import br.com.academy.wakanda.cadastro_saude.beneficiario.Application.api.BeneficiarioListResponse;
 import br.com.academy.wakanda.cadastro_saude.beneficiario.domain.Beneficiario;
+
+import java.util.List;
 
 public interface BeneficiarioRepository {
     Beneficiario salva(Beneficiario beneficiario);
+    List<Beneficiario> buscaTodosBeneficiarios();
+
 }
